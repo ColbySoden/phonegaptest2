3e84aaf8506f6805d9d50200ff136408aa4a98f9
Colby Soden <colby.soden@gmail.com>
2013-07-20 20:25:37 -0500

    0.12
    -Actually fixed screen size

--- phonegap-workshop-master/phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/phonegap-workshop-master/js/main.js
@@ -28,12 +28,4 @@ function tabview_switch(TabViewId, id) { tabview_aux(TabViewId, id); }
 
 function tabview_initialize(TabViewId) { tabview_aux(TabViewId,  1); }
 
-/* important! for alignment, you should make things
- * relative to the canvas' current width/height.
- */
-function draw() {
-  var ctx = (a canvas context);
-  ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-  //...drawing code...
-}
+
